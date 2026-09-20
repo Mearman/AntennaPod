355407ccd8ba76a574dfa1b9fb10b1ffcd5daccb
Joseph Mearman <joseph@mearman.co.uk>
2026-09-20 12:58:11 +0100

    test: remove duplicate try-with-resources release test

--- model/src/test/java/de/danoeh/antennapod/model/MediaMetadataRetrieverCompatTest.java
+++ model/src/test/java/de/danoeh/antennapod/model/MediaMetadataRetrieverCompatTest.java
@@ -7,7 +7,6 @@ import org.robolectric.RobolectricTestRunner;
 import java.io.IOException;
 
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 
@@ -26,15 +25,6 @@ public class MediaMetadataRetrieverCompatTest {
         MediaMetadataRetrieverCompat retriever = spy(new MediaMetadataRetrieverCompat());
         doThrow(new IOException("release failed")).when(retriever).release();
         retriever.close();
-        verify(retriever).release();
-    }
-
-    @Test
-    public void tryWithResources_releasesRetrieverOnExit() throws IOException {
-        MediaMetadataRetrieverCompat retriever = spy(new MediaMetadataRetrieverCompat());
-        try (MediaMetadataRetrieverCompat resource = retriever) {
-            verify(resource, never()).release();
-        }
         verify(retriever).release();
     }
 }
