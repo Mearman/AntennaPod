8490c4cbb4294c7c33e5f6adb1820f5926761c6e
Joseph Mearman <joseph@mearman.co.uk>
2026-09-20 14:57:23 +0100

    Assert which podcasts the top list keeps when filtering subscribed and author-less entries

--- net/discovery/src/test/java/de/danoeh/antennapod/net/discovery/ItunesTopListLoaderIntegrationTest.java
+++ net/discovery/src/test/java/de/danoeh/antennapod/net/discovery/ItunesTopListLoaderIntegrationTest.java
@@ -120,6 +120,8 @@ public class ItunesTopListLoaderIntegrationTest extends SearcherTestBase {
                 Arrays.asList(notSubscribed, withoutAuthor));
 
         assertEquals(2, results.size());
+        assertEquals("Archived", results.get(0).title);
+        assertEquals("Untitled author", results.get(1).title);
     }
 
     @Test
