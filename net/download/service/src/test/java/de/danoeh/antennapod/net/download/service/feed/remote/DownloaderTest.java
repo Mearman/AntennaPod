ae16707d9a42a34eac91298ae06f7f4ce5d4e8bb
Joseph Mearman <joseph@mearman.co.uk>
2026-09-20 12:52:46 +0100

    Test downloader lifecycle state

---
+++ net/download/service/src/test/java/de/danoeh/antennapod/net/download/service/feed/remote/DownloaderTest.java
@@ -0,0 +1,76 @@
+package de.danoeh.antennapod.net.download.service.feed.remote;
+
+import android.os.Bundle;
+import de.danoeh.antennapod.model.download.DownloadRequest;
+import de.danoeh.antennapod.model.feed.FeedMedia;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
+public class DownloaderTest {
+    private static class CountingDownloader extends Downloader {
+        private int downloads = 0;
+
+        CountingDownloader(DownloadRequest request) {
+            super(request);
+        }
+
+        @Override
+        protected void download() {
+            downloads++;
+        }
+    }
+
+    private static DownloadRequest request() {
+        return new DownloadRequest("/downloads/file", "http://example.com/a.mp3", "Episode", 12,
+                FeedMedia.FEEDFILETYPE_FEEDMEDIA, null, null, null, false, new Bundle(), true);
+    }
+
+    @Test
+    public void resultStartsUnsuccessfulAndDescribesTheRequest() {
+        Downloader downloader = new CountingDownloader(request());
+        assertFalse(downloader.getResult().isSuccessful());
+        assertEquals("Episode", downloader.getResult().getTitle());
+        assertEquals(12, downloader.getResult().getFeedfileId());
+        assertEquals(FeedMedia.FEEDFILETYPE_FEEDMEDIA, downloader.getResult().getFeedfileType());
+    }
+
+    @Test
+    public void downloaderStartsNeitherFinishedNorCancelled() {
+        Downloader downloader = new CountingDownloader(request());
+        assertFalse(downloader.isFinished());
+        assertFalse(downloader.cancelled);
+        assertNull(downloader.permanentRedirectUrl);
+    }
+
+    @Test
+    public void callRunsTheDownloadOnceAndMarksItFinished() {
+        CountingDownloader downloader = new CountingDownloader(request());
+
+        Downloader returned = downloader.call();
+
+        assertSame(downloader, returned);
+        assertEquals(1, downloader.downloads);
+        assertTrue(downloader.isFinished());
+    }
+
+    @Test
+    public void cancelMarksTheDownloaderCancelled() {
+        Downloader downloader = new CountingDownloader(request());
+        downloader.cancel();
+        assertTrue(downloader.cancelled);
+    }
+
+    @Test
+    public void downloadRequestIsExposed() {
+        DownloadRequest request = request();
+        assertSame(request, new CountingDownloader(request).getDownloadRequest());
+    }
+}
