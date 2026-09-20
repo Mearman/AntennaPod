d12aa12e0fbe2772cc28519b71e13371fba5ee4c
Joseph Mearman <joseph@mearman.co.uk>
2026-09-20 13:15:20 +0100

    Assert that the main thread assertion does not throw in tests and reset the build fingerprint
    The fingerprint override is cleared after each test so it cannot leak into later tests.

--- system/src/test/java/de/danoeh/antennapod/system/utils/ThreadUtilsTest.java
+++ system/src/test/java/de/danoeh/antennapod/system/utils/ThreadUtilsTest.java
@@ -1,5 +1,6 @@
 package de.danoeh.antennapod.system.utils;
 
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
@@ -12,27 +13,35 @@ import static org.junit.Assert.assertNull;
 @RunWith(RobolectricTestRunner.class)
 public class ThreadUtilsTest {
 
-    @Test
-    public void mainThreadIsAllowedInsideTests() {
-        ThreadUtils.assertNotMainThread();
+    @After
+    public void resetBuild() {
+        ShadowBuild.reset();
+    }
+
+    private static RuntimeException failureOfAssertion() {
+        try {
+            ThreadUtils.assertNotMainThread();
+            return null;
+        } catch (RuntimeException e) {
+            return e;
+        }
     }
 
     @Test
-    public void mainThreadIsAllowedWhenJUnitIsOnClasspathOnNonRobolectricDevice() {
+    public void mainThreadIsNotRejectedInsideTests() {
+        assertNull(failureOfAssertion());
+    }
+
+    @Test
+    public void mainThreadIsNotRejectedWhenJUnitIsOnClasspathOnNonRobolectricDevice() {
         ShadowBuild.setFingerprint("release-keys/device");
-        ThreadUtils.assertNotMainThread();
+        assertNull(failureOfAssertion());
     }
 
     @Test
-    public void backgroundThreadIsAlwaysAllowed() throws InterruptedException {
-        AtomicReference<Throwable> failure = new AtomicReference<>();
-        Thread thread = new Thread(() -> {
-            try {
-                ThreadUtils.assertNotMainThread();
-            } catch (RuntimeException e) {
-                failure.set(e);
-            }
-        });
+    public void backgroundThreadIsNotRejected() throws InterruptedException {
+        AtomicReference<RuntimeException> failure = new AtomicReference<>();
+        Thread thread = new Thread(() -> failure.set(failureOfAssertion()));
         thread.start();
         thread.join();
         assertNull(failure.get());
