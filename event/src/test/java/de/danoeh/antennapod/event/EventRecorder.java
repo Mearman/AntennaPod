662683c53293e54c7e7bd722359129c46da76b68
Joseph Mearman <joseph@mearman.co.uk>
2026-09-20 14:25:24 +0100

    Test that queue changes publish queue events matching the stored queue

---
+++ event/src/test/java/de/danoeh/antennapod/event/EventRecorder.java
@@ -0,0 +1,74 @@
+package de.danoeh.antennapod.event;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventRecorder {
+    private final List<Object> received = new ArrayList<>();
+
+    void register() {
+        EventBus.getDefault().register(this);
+    }
+
+    void unregister() {
+        if (EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().unregister(this);
+        }
+    }
+
+    void clear() {
+        received.clear();
+    }
+
+    @Subscribe
+    public void onQueueEvent(QueueEvent event) {
+        received.add(event);
+    }
+
+    @Subscribe
+    public void onFeedItemEvent(FeedItemEvent event) {
+        received.add(event);
+    }
+
+    @Subscribe
+    public void onFeedEvent(FeedEvent event) {
+        received.add(event);
+    }
+
+    @Subscribe
+    public void onFeedListUpdateEvent(FeedListUpdateEvent event) {
+        received.add(event);
+    }
+
+    @Subscribe
+    public void onEpisodeDownloadEvent(EpisodeDownloadEvent event) {
+        received.add(event);
+    }
+
+    @Subscribe
+    public void onMessageEvent(MessageEvent event) {
+        received.add(event);
+    }
+
+    <T> List<T> of(Class<T> type) {
+        List<T> result = new ArrayList<>();
+        for (Object event : received) {
+            if (type.isInstance(event)) {
+                result.add(type.cast(event));
+            }
+        }
+        return result;
+    }
+
+    <T> T single(Class<T> type) {
+        List<T> matching = of(type);
+        if (matching.size() != 1) {
+            throw new AssertionError("Expected exactly one " + type.getSimpleName()
+                    + " but received " + matching.size());
+        }
+        return matching.get(0);
+    }
+}
