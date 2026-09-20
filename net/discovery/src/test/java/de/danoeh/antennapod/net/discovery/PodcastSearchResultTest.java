4fb0d7cc2a8f55770d7b06df975937a62628a924
Joseph Mearman <joseph@mearman.co.uk>
2026-09-20 13:03:55 +0100

    Drop tests that only restated constructor arguments, getters and class hierarchy
    The dummy search result test and the end to end lookup failure test keep covering the behaviour.

--- net/discovery/src/test/java/de/danoeh/antennapod/net/discovery/PodcastSearchResultTest.java
+++ net/discovery/src/test/java/de/danoeh/antennapod/net/discovery/PodcastSearchResultTest.java
@@ -3,28 +3,8 @@ package de.danoeh.antennapod.net.discovery;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 public class PodcastSearchResultTest {
-
-    @Test
-    public void testConstructorKeepsAllFields() {
-        PodcastSearchResult result = new PodcastSearchResult("Title", "https://img.example/a.jpg",
-                "https://feeds.example/a.xml", "Author");
-        assertEquals("Title", result.title);
-        assertEquals("https://img.example/a.jpg", result.imageUrl);
-        assertEquals("https://feeds.example/a.xml", result.feedUrl);
-        assertEquals("Author", result.author);
-    }
-
-    @Test
-    public void testOptionalFieldsCanBeNull() {
-        PodcastSearchResult result = new PodcastSearchResult("Title", null, null, null);
-        assertEquals("Title", result.title);
-        assertNull(result.imageUrl);
-        assertNull(result.feedUrl);
-        assertNull(result.author);
-    }
 
     @Test
     public void testDummyHasEmptyFieldsInsteadOfNulls() {
