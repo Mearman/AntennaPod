03b698dbff70f9c8d275f6b1e0397ecaffde8c5e
Joseph Mearman <joseph@mearman.co.uk>
2026-09-20 14:56:05 +0100

    Test applying a feed volume setting during playback and Wear action extras

---
+++ playback/service/src/test/java/de/danoeh/antennapod/playback/service/internal/PlaybackVolumeUpdateTest.java
@@ -0,0 +1,164 @@
+package de.danoeh.antennapod.playback.service.internal;
+
+import android.content.Context;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import de.danoeh.antennapod.model.feed.Feed;
+import de.danoeh.antennapod.model.feed.FeedMedia;
+import de.danoeh.antennapod.model.feed.VolumeAdaptionSetting;
+import de.danoeh.antennapod.model.playback.MediaType;
+import de.danoeh.antennapod.model.playback.Playable;
+import de.danoeh.antennapod.playback.base.PlaybackServiceMediaPlayer;
+import de.danoeh.antennapod.playback.base.PlayerStatus;
+import de.danoeh.antennapod.storage.database.DBWriter;
+import de.danoeh.antennapod.test.categories.IntegrationTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.Assert.assertEquals;
+
+@Category(IntegrationTest.class)
+@RunWith(RobolectricTestRunner.class)
+public class PlaybackVolumeUpdateTest {
+    private static final String FEED_URL = "http://example.com/feed";
+    private Context context;
+    private Feed feed;
+    private LocalPSMP player;
+    private PlaybackVolumeUpdater updater;
+
+    @Before
+    public void setUp() {
+        context = RuntimeEnvironment.getApplication();
+        PlaybackTestDatabase.setUp(context);
+        feed = PlaybackTestDatabase.storeFeed(context, FEED_URL, "Main", 1);
+        player = new LocalPSMP(context, new IgnoringCallback());
+        updater = new PlaybackVolumeUpdater();
+    }
+
+    @After
+    public void tearDown() {
+        player.shutdown();
+        PlaybackTestDatabase.tearDown();
+    }
+
+    private FeedMedia downloadedMedia() throws IOException, ExecutionException, InterruptedException {
+        File file = new File(context.getCacheDir(), "episode.mp3");
+        try (FileOutputStream out = new FileOutputStream(file)) {
+            out.write(new byte[]{0, 0, 0, 0});
+        }
+        FeedMedia media = PlaybackTestDatabase.storedItems(feed.getId()).get(0).getMedia();
+        media.setLocalFileUrl(file.getAbsolutePath());
+        media.setDownloaded(true, System.currentTimeMillis());
+        DBWriter.setFeedMedia(media).get();
+        return PlaybackTestDatabase.storedItems(feed.getId()).get(0).getMedia();
+    }
+
+    @Test
+    public void changingTheVolumeSettingOfTheFeedBeingPlayedAppliesItAndKeepsPlaying()
+            throws IOException, ExecutionException, InterruptedException {
+        FeedMedia media = downloadedMedia();
+        player.playMediaObject(media, false, true, true);
+
+        updater.updateVolumeIfNecessary(player, feed.getId(), VolumeAdaptionSetting.HEAVY_REDUCTION);
+
+        assertEquals(VolumeAdaptionSetting.HEAVY_REDUCTION,
+                media.getItem().getFeed().getPreferences().getVolumeAdaptionSetting());
+        assertEquals(PlayerStatus.PLAYING, player.getPlayerStatus());
+    }
+
+    @Test
+    public void changingTheVolumeSettingOfAPausedEpisodeLeavesItPaused()
+            throws IOException, ExecutionException, InterruptedException {
+        FeedMedia media = downloadedMedia();
+        player.playMediaObject(media, false, false, true);
+
+        updater.updateVolumeIfNecessary(player, feed.getId(), VolumeAdaptionSetting.LIGHT_BOOST);
+
+        assertEquals(VolumeAdaptionSetting.LIGHT_BOOST,
+                media.getItem().getFeed().getPreferences().getVolumeAdaptionSetting());
+        assertEquals(PlayerStatus.PREPARED, player.getPlayerStatus());
+    }
+
+    @Test
+    public void changingTheVolumeSettingOfAnotherFeedLeavesTheCurrentEpisodeAlone()
+            throws IOException, ExecutionException, InterruptedException {
+        FeedMedia media = downloadedMedia();
+        VolumeAdaptionSetting before = media.getItem().getFeed().getPreferences().getVolumeAdaptionSetting();
+        player.playMediaObject(media, false, true, true);
+
+        updater.updateVolumeIfNecessary(player, feed.getId() + 1, VolumeAdaptionSetting.HEAVY_BOOST);
+
+        assertEquals(before, media.getItem().getFeed().getPreferences().getVolumeAdaptionSetting());
+    }
+
+    @Test
+    public void changingTheVolumeSettingWhileNothingIsLoadedIsIgnored() {
+        updater.updateVolumeIfNecessary(player, feed.getId(), VolumeAdaptionSetting.HEAVY_BOOST);
+
+        assertEquals(PlayerStatus.STOPPED, player.getPlayerStatus());
+    }
+
+    private static class IgnoringCallback implements PlaybackServiceMediaPlayer.PSMPCallback {
+        @Override
+        public void statusChanged(PlaybackServiceMediaPlayer.PSMPInfo newInfo) {
+        }
+
+        @Override
+        public void shouldStop() {
+        }
+
+        @Override
+        public void episodeFinishedPlayback() {
+        }
+
+        @Override
+        public boolean shouldContinueToNextEpisode() {
+            return false;
+        }
+
+        @Override
+        public void onMediaChanged(boolean reloadUI) {
+        }
+
+        @Override
+        public void onPostPlayback(@NonNull Playable media, boolean ended, boolean skipped, boolean playingNext) {
+        }
+
+        @Override
+        public void onPlaybackStart(@NonNull Playable playable, int position) {
+        }
+
+        @Override
+        public void onPlaybackPause(Playable playable, int position) {
+        }
+
+        @Override
+        public Playable getNextInQueue(Playable currentMedia) {
+            return null;
+        }
+
+        @Nullable
+        @Override
+        public Playable findMedia(@NonNull String url) {
+            return null;
+        }
+
+        @Override
+        public void onPlaybackEnded(MediaType mediaType, boolean stopPlaying) {
+        }
+
+        @Override
+        public void ensureMediaInfoLoaded(@NonNull Playable media) {
+        }
+    }
+}
---
+++ playback/service/src/test/java/de/danoeh/antennapod/playback/service/internal/WearMediaSessionTest.java
@@ -0,0 +1,47 @@
+package de.danoeh.antennapod.playback.service.internal;
+
+import android.content.Context;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import de.danoeh.antennapod.test.categories.IntegrationTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+
+import static org.junit.Assert.assertTrue;
+
+@Category(IntegrationTest.class)
+@RunWith(RobolectricTestRunner.class)
+public class WearMediaSessionTest {
+    private static final String SHOW_ON_WEAR =
+            "android.support.wearable.media.extra.CUSTOM_ACTION_SHOW_ON_WEAR";
+    private MediaSessionCompat session;
+
+    @Before
+    public void setUp() {
+        Context context = RuntimeEnvironment.getApplication();
+        PlaybackTestDatabase.setUp(context);
+        session = new MediaSessionCompat(context, "WearMediaSessionTest");
+    }
+
+    @After
+    public void tearDown() {
+        session.release();
+        PlaybackTestDatabase.tearDown();
+    }
+
+    @Test
+    public void aCustomActionIsMarkedToShowOnTheWatch() {
+        PlaybackStateCompat.CustomAction.Builder builder = new PlaybackStateCompat.CustomAction.Builder(
+                "rewind", "Rewind", android.R.drawable.ic_media_rew);
+
+        WearMediaSession.addWearExtrasToAction(builder);
+
+        assertTrue(builder.build().getExtras().getBoolean(SHOW_ON_WEAR));
+    }
+
+}
