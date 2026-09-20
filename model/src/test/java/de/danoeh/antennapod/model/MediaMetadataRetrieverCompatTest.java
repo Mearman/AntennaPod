6a5f7fc70b23433b222190e6e53a94a4e246c9cf
Joseph Mearman <joseph@mearman.co.uk>
2026-09-20 12:53:56 +0100

    Test retriever close releases and swallows release failures

---
+++ model/src/test/java/de/danoeh/antennapod/model/MediaMetadataRetrieverCompatTest.java
@@ -0,0 +1,40 @@
+package de.danoeh.antennapod.model;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+@RunWith(RobolectricTestRunner.class)
+public class MediaMetadataRetrieverCompatTest {
+
+    @Test
+    public void close_releasesRetriever() throws IOException {
+        MediaMetadataRetrieverCompat retriever = spy(new MediaMetadataRetrieverCompat());
+        retriever.close();
+        verify(retriever).release();
+    }
+
+    @Test
+    public void close_swallowsIoExceptionFromRelease() throws IOException {
+        MediaMetadataRetrieverCompat retriever = spy(new MediaMetadataRetrieverCompat());
+        doThrow(new IOException("release failed")).when(retriever).release();
+        retriever.close();
+        verify(retriever).release();
+    }
+
+    @Test
+    public void tryWithResources_releasesRetrieverOnExit() throws IOException {
+        MediaMetadataRetrieverCompat retriever = spy(new MediaMetadataRetrieverCompat());
+        try (MediaMetadataRetrieverCompat resource = retriever) {
+            verify(resource, never()).release();
+        }
+        verify(retriever).release();
+    }
+}
