36d6893c5702257fb71ebc62e2d03026f64321db
Joseph Mearman <joseph@mearman.co.uk>
2026-09-20 13:12:04 +0100

    Remove tests that only restate constants or exercise getters and toString

--- net/sync/wear-interface/src/test/java/de/danoeh/antennapod/net/sync/wearinterface/WearDataPathsTest.java
+++ net/sync/wear-interface/src/test/java/de/danoeh/antennapod/net/sync/wearinterface/WearDataPathsTest.java
@@ -3,13 +3,10 @@ package de.danoeh.antennapod.net.sync.wearinterface;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
 
 public class WearDataPathsTest {
     @Test
     public void playPathAppendsItemIdToPlayPrefix() {
-        assertEquals(WearDataPaths.PLAY_PREFIX + 42, WearDataPaths.playPath(42));
         assertEquals("/play/42", WearDataPaths.playPath(42));
     }
 
@@ -21,17 +18,5 @@ public class WearDataPathsTest {
     @Test
     public void feedEpisodesPathAppendsFeedIdToItsPrefix() {
         assertEquals("/feed_episodes/13", WearDataPaths.feedEpisodesPath(13));
-    }
-
-    @Test
-    public void dynamicPathsAreRecognisedByTheirPrefixOnly() {
-        assertTrue(WearDataPaths.playPath(1).startsWith(WearDataPaths.PLAY_PREFIX));
-        assertTrue(WearDataPaths.openOnPhonePath(1).startsWith(WearDataPaths.OPEN_ON_PHONE_PREFIX));
-        assertTrue(WearDataPaths.feedEpisodesPath(1).startsWith(WearDataPaths.FEED_EPISODES_PREFIX));
-    }
-
-    @Test
-    public void differentItemsGetDifferentPaths() {
-        assertNotEquals(WearDataPaths.playPath(1), WearDataPaths.playPath(2));
     }
 }
